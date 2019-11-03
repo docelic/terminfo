cf0482bafb81a7dffb07203b0e776d56dcc23603
Davor Ocelic <docelic@crystallabs.io>
2019-11-03 03:10:59 +0100

    Terminfo improvements

--- tput_spec.cr
+++ tput_spec.cr
@@ -21,5 +21,41 @@ describe "Tput" do
   it "has fallback termcap" do
     ::Crysterm::Tput::Termcap.should be_a String
   end
+
+  # XXX does tput need to be a class? Can it be a module?
+  it "can instantiate" do
+    obj = ::Crysterm::Tput.new
+    obj.should be_truthy
+  end
+
+  it "can report term" do
+    obj = ::Crysterm::Tput.new(terminal="abcdef-256color")
+    obj.term("xterm").should be_false
+    obj.term("abcdef").should be_true
+    obj.term("abcdef").should be_true
+  end
+
+  it "can detect features" do
+    obj = ::Crysterm::Tput.new
+
+    obj.detect_magic_cookie.should be_true
+    obj.detect_padding.should be_true
+    obj.detect_setbuf.should be_true
+
+    ENV["NCURSES_NO_MAGIC_COOKIE"]="1"
+    ENV["NCURSES_NO_PADDING"]="1"
+    ENV["NCURSES_NO_SETBUF"]="1"
+
+    obj.detect_magic_cookie.should be_false
+    obj.detect_padding.should be_false
+    obj.detect_setbuf.should be_false
+
+    ENV.delete "NCURSES_NO_MAGIC_COOKIE"
+    ENV.delete "NCURSES_NO_PADDING"
+    ENV.delete "NCURSES_NO_SETBUF"
+
+    # TODO detect_unicode not tested currently
+
+  end
 end
 
