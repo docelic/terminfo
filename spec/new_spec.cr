4e624a2516ef78a5d6d46db9c6b51ab0595e41a4
Jason DeLeon <jason@progmem.space>
2020-05-05 16:09:36 -0400

    Add support for case-insensitive filesystems

--- spec/new_spec.cr
+++ spec/new_spec.cr
@@ -1,7 +1,7 @@
 require "./spec_helper"
 
 describe Terminfo do
-  data = Terminfo::Data.new path: "/lib/terminfo/x/xterm"
+  data = Terminfo::Data.new path: "/lib/terminfo/x/xterm" # Not accurate on macOS, fails
   data.name.should eq "xterm"
 
   data = Terminfo::Data.new term: "xterm-256color"
@@ -9,9 +9,6 @@ describe Terminfo do
 
   data = Terminfo::Data.new builtin: "windows-ansi"
   data.name.should eq "ansi"
-
-  data = Terminfo::Data.new
-  data.name.should eq "xterm"
 
   data = Terminfo::Data.new
   data.name.should eq "xterm"
