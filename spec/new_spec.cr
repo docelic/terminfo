7571bdc1ce0350886af613eee33464d8762accc7
Jason DeLeon <jason@progmem.space>
2020-05-05 17:16:44 -0400

    Leverage existing terminfo files for testing

--- spec/new_spec.cr
+++ spec/new_spec.cr
@@ -1,15 +1,20 @@
 require "./spec_helper"
 
 describe Terminfo do
-  data = Terminfo::Data.new path: "/lib/terminfo/x/xterm" # Not accurate on macOS, fails
+  # Load terminfo file
+  data = Terminfo::Data.new path: "./filesystem/xterm"
   data.name.should eq "xterm"
 
+  # Load terminfo from system
   data = Terminfo::Data.new term: "xterm-256color"
   data.name.should eq "xterm-256color"
 
+  # Load terminfo from built-in
   data = Terminfo::Data.new builtin: "windows-ansi"
   data.name.should eq "ansi"
 
+  # Load terminfo via autodetect
+  expected_term = ENV["TERM"] || "xterm"
   data = Terminfo::Data.new
-  data.name.should eq "xterm"
+  data.name.should eq expected_term
 end
